7ae808329ace7d24dd6e79558b9aec6331988720
Iftekharul <iftekharulislam778@gmail.com>
2026-09-13 11:43:31 +0600

    complete Nav component with customized styling and responsive menu

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Nav from './components/Nav';
+
+export default function App() {
+  return (
+    <div>
+        <Nav techCount={0} />
+    </div>
+  );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import logoText from '../assets/logo-text.png';
+import hamburgerIcon from '../assets/hamburger.png';
+import xmarkIcon from '../assets/xmark.png';
+import { useState } from 'react';
+
+interface NavProps {
+  techCount: number;
+}
+
+export default function Nav({ techCount }: NavProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="nav-container">
+      <div className="nav-left">
+        <img src={logoText} alt="MyDevStack Logo" className="logo" />
+      </div>
+
+      <button className="hamburger-btn" onClick={() => setIsOpen(!isOpen)}>
+        <img src={isOpen ? xmarkIcon : hamburgerIcon} alt="Toggle Menu" />
+      </button>
+
+      <div className={`nav-center ${isOpen ? 'open' : ''}`}>
+        <a href="home">Home</a>
+        <a href="technologies">Technologies</a>
+        <a href="about">About</a>
+      </div>
+
+      <div className="nav-right">
+        <button className="btn-signin">Sign In</button>
+        <button className="btn-signup">Sign Up</button>
+      </div>
+    </nav>
+  );
+}
